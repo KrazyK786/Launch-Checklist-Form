aa956eefaee70105b4694d8313743e4765472b18
Kristopher Johnson <krazyk786@gmail.com>
2019-10-16 18:48:13 -0500

    restructured validation to account for edge cases and cleaned up code

--- script.js
+++ script.js
@@ -15,9 +15,10 @@
 
 function init(){
    // declare variables
-   const formEle = document.getElementById("launchForm");  //getElementById("formContainer"); // 
+   const formEle = document.getElementById("launchForm");  
+   const button = document.getElementById("formSubmit");
    let pilotName = document.querySelector("input[name = pilotName]");
-   let copilotName = document.querySelector("input[name = copilotName]");// getElementById("copilotName");
+   let copilotName = document.querySelector("input[name = copilotName]");
    let fuelLevel = document.querySelector("input[name = fuelLevel]");
    let cargoMass = document.querySelector("input[name = cargoMass]");
    let pilotStatus = document.getElementById("pilotStatus");
@@ -27,83 +28,87 @@ function init(){
    let launchStatus = document.getElementById("launchStatus");
    let faultyItems = document.getElementById("faultyItems");
 
+   let origFuelStatus = fuelStatus.textContent;
+   let origCargoStatus = cargoStatus.textContent;
+
 
    // helper functions
-   function validate(value,expected){
+   function hasValue(value){
       let correct = true;
-      if (expected === "name"){
-         if (value === "" || !isNaN(value) ){
-            correct = false;
-         } 
-      } else if(expected === "number"){
-         if (value === "" || isNaN(value) ){
-            correct = false;
-         }
+      if(value === ""){
+         correct = false;
       }
-      
       return correct;
    }
 
+   // update heading of faultyItems 
    function notReady(){
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
    }
 
-   // submit event
-   formEle.addEventListener("submit",function(event){
-      console.log(typeof pilotName.value);
-      // pilot validation
-      if(!validate(pilotName.value,"name") ){
-         alert(`\'${pilotName.value}\' is not a valid value for Pilot Name.  Please try again!`);
+   function isValid(){
+      let correct = true;
+      if(!hasValue(pilotName.value) || !hasValue(copilotName.value) || !hasValue(fuelLevel.value) || !hasValue(cargoMass.value)){
+         alert("All fields are required!");
          event.preventDefault();
-      }
-
-      // copilot validation
-      else if(!validate(copilotName.value,"name")){
-         alert(`\'${copilotName.value}\' is not a valid value for Co-pilot Name.  Please try again!`);
-         event.preventDefault();
-         
+         correct = false;
       }
       
-      // fuelLevel validation
-      else if (!validate(fuelLevel.value,"number")){
-         alert(`\'${fuelLevel.value}\' is not a valid value for Fuel Level.  Please try again!`);
+      else if (!isNaN(pilotName.value) || !isNaN(copilotName.value) || pilotName.value < 1 || copilotName.value < 1 || fuelLevel.value < 0 || cargoMass.value < 0){
+         alert("Make sure to enter valid information for each field!");
          event.preventDefault();
-      }
-      
-      // cargoMass validdation
-      else if (!validate(cargoMass.value,"number")){
-         alert(`\'${cargoMass.value}\' is not a valid value for Cargo Mass.  Please try again!`);
-         event.preventDefault();
+         correct = false;
       }
 
+      return correct;
+   }
+
+   function showFaultyItems(){
+      let ready = true;
       // add pilot/copilot name to shuttle requirements
       pilotStatus.innerHTML = `${pilotName.value} ready`;
       copilotStatus.innerHTML = `${copilotName.value} ready`;
 
-      // update fuel level status if less than 10,000
+      // update fuel level status if less than 10,000 -else resets innerhtml in case form was resubmitted
       if (fuelLevel.value < 10000){
          fuelStatus.innerHTML = "There is not enough fuel for the journey";
-         // launchStatus.innerHTML = "Shuttle not ready for launch";
-         // launchStatus.style.color = "red";
          notReady();
+         ready = false;
+      }
+      else {
+         fuelStatus.innerHTML = origFuelStatus;
       }
 
-      // update cargo mass status if more than 10,000
-      else if (cargoMass.value > 10000){
+      // update cargo mass status if more than 10,000 -else resets innerhtml in case form was resubmitted
+      if (cargoMass.value > 10000){
          cargoStatus.innerHTML = "There is too much mass for the shuttle to take off";
          notReady();
+         ready = false;
+      }
+      else {
+         cargoStatus.innerHTML = origCargoStatus;
       }
 
-      else {
+      if (ready) {
          launchStatus.innerHTML = "Shuttle is ready for launch!";
          launchStatus.style.color = "green";
       }
 
-
       // unhide faulty items
       faultyItems.style.visibility = "visible";
-      event.preventDefault();  //for testing?
+      event.preventDefault();
+   }
+
+
+   // submit event
+   formEle.addEventListener("submit",function(event){
+      
+      // perform validation check
+      if (isValid()){
+         // update/display faulty items section
+         showFaultyItems();
+      }
    });
 
 }
